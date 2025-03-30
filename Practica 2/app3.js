656d742151a871907d4652e7eea7637eb4cfb305
AriasRamos <ariasramos912@gmail.com>
2025-03-30 18:47:24 -0400

    Add files via upload

---
+++ Practica 2/app3.js
@@ -0,0 +1,18 @@
+const mysql2 = require('mysql2');
+
+// Create a connection pool
+const pool = mysql2.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'testdb',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+
+// Query the database using a pooled connection
+pool.query('SELECT * FROM users', (err, results, fields) => {
+  if (err) throw err;
+  console.log(results);
+});
